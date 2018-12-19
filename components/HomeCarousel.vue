b7f0f428d262bd333ab9fe7a2d7c350d91fca529
Donaven Snowden <donaven.snowden@gmail.com>
2018-12-19 07:09:00 -0800

    fix link in home page

--- components/HomeCarousel.vue
+++ components/HomeCarousel.vue
@@ -7,7 +7,7 @@
         <img src="~assets/images/home/slide_2.jpg" alt="Endless Photo Booths Slide Two" class="card-img-top zoomable-img">
       </div>
       <div class="card border-0">
-        <img src="~assets/images/home/slide_1.jpg" alt="Endless Photo Booths Slide Three" class="card-img-top zoomable-img">
+        <img src="~assets/images/home/slide_3.jpg" alt="Endless Photo Booths Slide Three" class="card-img-top zoomable-img">
       </div>
       <div class="card border-0">
         <img src="~assets/images/home/slide_4.png" alt="Endless Photo Booths Slide Four" class="card-img-top zoomable-img">
