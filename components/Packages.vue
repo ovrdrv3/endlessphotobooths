3fa96d87c43b67fab59fe1879d7c1829a0bc26bf
Donaven Snowden <donaven.snowden@gmail.com>
2018-12-21 02:01:30 -0800

    decrease spacing between packages

--- components/Packages.vue
+++ components/Packages.vue
@@ -5,50 +5,59 @@
             <b-card-group deck>
                 <b-card title="Package I">
                     <b-list-group flush>
-                        <b-list-group-item>Rose Gold, Red, Silver backdrop</b-list-group-item>
-                        <b-list-group-item>2 Strips with 3 Photos Taken</b-list-group-item>
-                        <b-list-group-item>Copies for Everyone in Photo</b-list-group-item>
-                        <b-list-group-item>Unlimited Use of Booth</b-list-group-item>
-                        <b-list-group-item>Open air Photo Booth</b-list-group-item>
-                        <b-list-group-item>Custom Text For Bottom Of Picture</b-list-group-item>
-                        <b-list-group-item>Props</b-list-group-item>
-                        <b-list-group-item>Person to Run Photo Booth</b-list-group-item>
-                        <b-list-group-item>Drop off and Pickup Included</b-list-group-item>
+                        <b-list-group-item class="py-1">Rose Gold, Red, Silver backdrop</b-list-group-item>
+                        <b-list-group-item class="py-1">2 Strips with 3 Photos Taken</b-list-group-item>
+                        <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
+                        <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Open air Photo Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
+                        <b-list-group-item class="py-1">Props</b-list-group-item>
+                        <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Drop off and Pickup Included</b-list-group-item>
                     </b-list-group>
-                    <b-button to="/contact-us"
-                              variant="primary">Book
+                    <b-button slot="footer"
+                              to="/contact-us"
+                              variant="primary"
+                              block>
+                              Book
                     </b-button>
                 </b-card>
                 <b-card title="Package II">
                     <b-list-group flush>
-                        <b-list-group-item>LED Enclosed Booth</b-list-group-item>
-                        <b-list-group-item>2 Strips with 3 Photos Taken</b-list-group-item>
-                        <b-list-group-item>Photo Album</b-list-group-item>
-                        <b-list-group-item>Copies for Everyone in Photo</b-list-group-item>
-                        <b-list-group-item>Unlimited Use of Booth</b-list-group-item>
-                        <b-list-group-item>Custom Text For Bottom Of Picture</b-list-group-item>
-                        <b-list-group-item>Props</b-list-group-item>
-                        <b-list-group-item>Person to Run Photo Booth</b-list-group-item>
-                        <b-list-group-item>Drop off and Pickup Included</b-list-group-item>
+                        <b-list-group-item class="py-1">LED Enclosed Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">2 Strips with 3 Photos Taken</b-list-group-item>
+                        <b-list-group-item class="py-1">Photo Album</b-list-group-item>
+                        <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
+                        <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
+                        <b-list-group-item class="py-1">Props</b-list-group-item>
+                        <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Drop off and Pickup Included</b-list-group-item>
                     </b-list-group>
-                    <b-button to="/contact-us"
-                              variant="primary">Book
+                    <b-button slot="footer"
+                              to="/contact-us"
+                              variant="primary"
+                              block>
+                              Book
                     </b-button>
                 </b-card>
                 <b-card title="Package III">
                     <b-list-group flush>
-                        <b-list-group-item>LED Enclosed Booth</b-list-group-item>
-                        <b-list-group-item>Full 4x6 Prints</b-list-group-item>
-                        <b-list-group-item>Photo Album</b-list-group-item>
-                        <b-list-group-item>Copies for Everyone in Photo</b-list-group-item>
-                        <b-list-group-item>Unlimited Use of Booth</b-list-group-item>
-                        <b-list-group-item>Custom Text For Bottom Of Picture</b-list-group-item>
-                        <b-list-group-item>Props</b-list-group-item>
-                        <b-list-group-item>Person to Run Photo Booth</b-list-group-item>
-                        <b-list-group-item>Drop off and Pickup Included</b-list-group-item>
+                        <b-list-group-item class="py-1">LED Enclosed Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Full 4x6 Prints</b-list-group-item>
+                        <b-list-group-item class="py-1">Photo Album</b-list-group-item>
+                        <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
+                        <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
+                        <b-list-group-item class="py-1">Props</b-list-group-item>
+                        <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
+                        <b-list-group-item class="py-1">Drop off and Pickup Included</b-list-group-item>
                     </b-list-group>
-                    <b-button to="/contact-us"
-                              variant="primary">Book
+                    <b-button slot="footer"
+                              to="/contact-us"
+                              variant="primary"
+                              block>
+                              Book
                     </b-button>
                 </b-card>
             </b-card-group>
