765acfabe999c50a4c716049d84ad9ccff083353
Donaven Snowden <donaven.snowden@gmail.com>
2019-03-12 00:24:25 -0700

    update index

--- components/Packages.vue
+++ components/Packages.vue
@@ -7,7 +7,7 @@
                 <b-list-group-item class="py-1">2 Strips with 3 Photos Taken</b-list-group-item>
                 <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
                 <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
-                <b-list-group-item class="py-1">Open air Photo Booth</b-list-group-item>
+                <b-list-group-item class="py-1">Open Air Photo Booth</b-list-group-item>
                 <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
                 <b-list-group-item class="py-1">Props</b-list-group-item>
                 <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
@@ -22,15 +22,9 @@
         </b-card>
         <b-card title="LED Booth">
             <b-list-group flush>
-                <b-list-group-item class="py-1">LED Enclosed Booth</b-list-group-item>
-                <b-list-group-item class="py-1">2 Strips with 3 Photos Taken</b-list-group-item>
+                <b-list-group-item class="py-1">All of Open Air Package, BUT</b-list-group-item>
+                <b-list-group-item class="py-1">LED Enclosed Booth instead of Open Air</b-list-group-item>
                 <b-list-group-item class="py-1">Photo Album</b-list-group-item>
-                <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
-                <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
-                <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
-                <b-list-group-item class="py-1">Props</b-list-group-item>
-                <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
-                <b-list-group-item class="py-1">Drop off and Pickup Included</b-list-group-item>
             </b-list-group>
             <b-button slot="footer"
                       to="/contact-us?package=II"
@@ -41,15 +35,8 @@
         </b-card>
         <b-card title="ENDLESS VIP">
             <b-list-group flush>
-                <b-list-group-item class="py-1">LED Enclosed Booth</b-list-group-item>
+                <b-list-group-item class="py-1">All of LED Booth Package, PLUS</b-list-group-item>
                 <b-list-group-item class="py-1">Full 4x6 Prints</b-list-group-item>
-                <b-list-group-item class="py-1">Photo Album</b-list-group-item>
-                <b-list-group-item class="py-1">Copies for Everyone in Photo</b-list-group-item>
-                <b-list-group-item class="py-1">Unlimited Use of Booth</b-list-group-item>
-                <b-list-group-item class="py-1">Custom Text For Bottom Of Picture</b-list-group-item>
-                <b-list-group-item class="py-1">Props</b-list-group-item>
-                <b-list-group-item class="py-1">Person to Run Photo Booth</b-list-group-item>
-                <b-list-group-item class="py-1">Drop off and Pickup Included</b-list-group-item>
             </b-list-group>
             <b-button slot="footer"
                       to="/contact-us?package=III"
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,42 @@
 <template>
   <b-container fluid>
       <h1 class="title">ENDLESS PHOTO{{spaceForMobile}}BOOTHS</h1>
-      <div class="subtitle">Ventura County Photo Booth Rentals</div>
+      <h5 class="subtitle">Ventura County Photo Booth Rentals</h5>
     <b-row class="py-3">
       <b-col>
-        <HomeCarousel/>
+        <!-- <HomeCarousel/> -->
+        <!-- <div class="parallax-text title"><h2>Living To Make Your Event Memorable - One Shot at a Time</h2></div> -->
+        <div v-if="!isMobile" class="parallax-text title"><h1 class="gray-text">LIVING TO MAKE YOUR EVENT MEMORABLE</h1><br><h1 class="gray-text">ONE SHOT AT A TIME</h1></div>
+        <parallax v-if="!isMobile" :speed-factor="0.1">
+          <picture>
+            <source srcset="~assets/images/home/home_splash-min.webp" type="image/webp" class="img-fluid dark-overlay">
+            <img src="~assets/images/home/home_splash-min.jpeg" alt="Endless Photo Booths Slide Two" class="img-fluid dark-overlay">
+          </picture>
+        </parallax>
+
+        <picture v-else>
+          <source srcset="~assets/images/home/home_splash-min.webp" type="image/webp" class="img-fluid">
+          <img src="~assets/images/home/home_splash-min.jpeg" alt="Endless Photo Booths Slide Two" class="img-fluid">
+        </picture>
+        <div v-if="isMobile">
+          <br>
+          <h5 class="subtitle">Living To Make Your Event Memorable - One Shot At a Time</h5>
+        </div>
       </b-col>
     </b-row>
-
+    <!-- Instagram and Yelp Logos -->
+    <b-row class="py-0">
+      <b-col md="10" offset-md="1">
+        <div class="d-flex justify-content-center">
+          <a href="https://www.instagram.com/endlessphotobooths">
+            <img src="~assets/images/home/instagram.png" alt="Instagram Link" style="width: 45px; height: 45px; top: 8px; position: relative;"/>
+          </a>
+          <a href="https://www.yelp.com/biz/endless-photobooth-oxnard">
+            <img src="~assets/images/home/yelp.png" alt="Yelp Link" class="logo" style="width: 60px; height: 60px; position: relative;"/>
+          </a>
+        </div>
+      </b-col>
+    </b-row>
     <!-- Make a non fluid container for the rest of the content -->
     <!-- Home info section -->
     <b-row class="py-3">
@@ -32,8 +61,7 @@
 
         <picture>
          <source class="d-block img-fluid zoomable-img" srcset="~assets/images/home/all_include-min.webp" type="image/webp">
-         <source class="d-block img-fluid zoomable-img" srcset="~assets/images/home/all_include-min.jpg" type="image/jpeg">
-          <img class="d-block img-fluid zoomable-img"
+         <img class="d-block img-fluid zoomable-img"
                src="~assets/images/home/all_include-min.jpg"
                alt="All Photo Booths Include Props, Different backdrop options, Person to Run Photo Booth, and more!">
         </picture>
@@ -67,9 +95,10 @@
 </template>
 
 <script>
-import HomeCarousel from '~/components/HomeCarousel.vue';
+// import HomeCarousel from '~/components/HomeCarousel.vue';
 import PhotoBoothTypes from '~/components/PhotoBoothTypes.vue';
 import mediumZoom from 'medium-zoom';
+import Parallax from 'vue-parallaxy';
 
 export default {
   head () {
@@ -81,26 +110,81 @@ export default {
     }
   },
   components: {
-    HomeCarousel,
-    PhotoBoothTypes
+    PhotoBoothTypes,
+    Parallax
   },
   mounted(){
     mediumZoom('.zoomable-img')
   },
   computed: {
+    spaceForMobile: function(){
+      return this.isMobile ? ' ' : ''
+    },
     // Check if mobile
-    spaceForMobile: function () {
+    isMobile: function () {
       var check = false;
 
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
 
-        return check ? ' ' : '';
+        return check;
     }
   }
 }
 </script>
 
 <style>
+
+.logo{
+    width: 60px;
+    height: 60px;
+    /*margin: auto;*/
+    /*left: 50%;*/
+    /*position: absolute;*/
+}
+
+.logo-list{
+  display: block;
+}
+
+.dark-overlay {
+  /*background: rgba(0, 0, 0, .6);*/
+  filter: brightness(70%);
+}
+
+.blue-text{
+  /*color: #317FBC;*/
+  color: #EEE;
+  background: #317FBC;
+  display:inline;
+  font-weight: 300;
+  text-align: center;
+}
+
+.gray-text{
+  color: #EEE;
+}
+
+.mobile-gray-text {
+  color: #EEE;
+  background: #317FBC;
+  /*display: block;*/
+  display:inline;
+  font-weight: 300;
+  text-align: center;
+  word-spacing: 5px;
+  text-align: center;
+}
+
+.parallax-text{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /*background-color: #eee;*/
+  opacity: 0.75;
+  filter: alpha(opacity=75);
+}
+
 .title {
   letter-spacing: 4px;
   display: block;
