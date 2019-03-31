32d2ed567b5e1bbc559d66640f9abae96e7ccab8
Donaven Snowden <donaven.snowden@gmail.com>
2019-03-31 12:03:25 -0700

    update contact form try 3

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -18,7 +18,27 @@
           <a href="mailto:endlessphotobooths@gmail.com?subject=Photo%20Booth%20Inquiry">endlessphotobooths@gmail.com</a>
         </div>
 
-        <ContactForm/>
+        <!-- <ContactForm/> -->
+            <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
 
 
   </b-container>
