689576549b3a932cbeff8e0f83be1fd64122ff32
Donaven Snowden <donaven.snowden@gmail.com>
2019-04-10 06:56:26 -0700

    add favicons for apple, update choose a template page

--- pages/choose-a-template.vue
+++ pages/choose-a-template.vue
@@ -1,17 +1,9 @@
-
-<div id="paddee-container" data-key="f89Okf2_1101" data-tags="landscape3l,landscape3p,landscape1l,portrait1p,strip3l" data-categories="">
-</div>
-<div style="text-align: center; padding-top: 30px;">Powered by <a href="http://getpaddee.com"><em>Paddee</em></a></div>
-<script src="https://paddee.herokuapp.com/widget_v1.js"></script>
-
-
 <template>
   <b-container>
 
     <h1 class="title">ENDLESS PHOTO{{spaceForMobile}}BOOTHS</h1>
     <div id="paddee-container" data-key="f89Okf2_1101" data-tags="landscape3l,landscape3p,landscape1l,portrait1p,strip3l" data-categories="">
     </div>
-    <script src="https://paddee.herokuapp.com/widget_v1.js"></script>
 
   </b-container>
 </template>
@@ -24,6 +16,9 @@ export default {
       title: 'Choose a Template',
       meta: [
         { hid: 'description', name: 'description', content: 'ENDLESS PHOTOBOOTHS, Ventura Photo Booth Rentals Choose a Template Page' }
+      ],
+      script: [
+        { src: 'https://paddee.herokuapp.com/widget_v1.js' }
       ]
     }
   },
