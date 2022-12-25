fa56b5ccdd02e5ad23ce796f589619e4080ed17c
Donaven Snowden <donaven.snowden@gmail.com>
2022-12-25 14:18:13 -0800

    add es-lint

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,34 @@
     <div class="video-bg-height">
       <div class="video-bg">
         <!-- This div is  intentionally blank. It creates the transparent black overlay over the video which you can modify in the CSS -->
-        <div class="overlay"></div>
+        <div class="overlay" />
 
-        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-          <source src="~assets/video/epb360video.mp4" type="video/mp4">
+        <video
+          playsinline="playsinline"
+          autoplay="autoplay"
+          muted="muted"
+          loop="loop"
+        >
+          <source
+            src="~assets/video/epb360video.mp4"
+            type="video/mp4"
+          >
         </video>
         <div class="container h-100">
           <div class="d-flex h-100 text-center">
             <div class="middle-text">
-              <h1 class="title fancy-font gray-text pt-150">ENDLESS PHOTOBOOTH</h1>
-              <h2 class="caption-text pt-150">LIVING TO MAKE YOUR EVENT MEMORABLE</h2>
-              <h2 class="caption-text">ONE SHOT AT A TIME</h2>
-              <h2 class="fancy-font caption-text pt-150">SANTA&nbspBARBARA • VENTURA • LOS&nbspANGELES</h2>
+              <h1 class="title fancy-font gray-text pt-150">
+                ENDLESS PHOTOBOOTH
+              </h1>
+              <h2 class="caption-text pt-150">
+                LIVING TO MAKE YOUR EVENT MEMORABLE
+              </h2>
+              <h2 class="caption-text">
+                ONE SHOT AT A TIME
+              </h2>
+              <h2 class="fancy-font caption-text pt-150">
+                SANTA&nbspBARBARA • VENTURA • LOS&nbspANGELES
+              </h2>
             </div>
           </div>
         </div>
@@ -22,17 +38,22 @@
     </div>
     <b-container class="copy">
       <insta-feed
-      container-class="image-container"
-      :mediatypes="['IMAGE']"
-      class="py-3"
+        container-class="image-container"
+        :mediatypes="['IMAGE']"
+        class="py-3"
       >
-        <template v-slot:loading="props">
-          <h1 v-if="props.loading" class="copy-heading pt-3">Loading recent instagram events...</h1>
+        <template #loading="props">
+          <h1
+            v-if="props.loading"
+            class="copy-heading pt-3"
+          >
+            Loading recent instagram events...
+          </h1>
         </template>
-        <template v-slot:feeds="props">
+        <template #feeds="props">
           <b-card
-            no-body
             v-if="props.feed.media_url.includes('.mp4')"
+            no-body
             class="p-0 mb-2 border-rounded"
           >
             <video
@@ -41,11 +62,15 @@
               loop
               muted
               class="w-100"
-              >
-            </video>
+            />
             <b-card-text class="p-2 caption-text">
-              {{props.feed.caption.replace(/•/g, '').replace(/#[a-zA-Z0-9]+/g, '')}}
-              <a :href="props.feed.permalink" rel="noopener" target="_blank" class="stretched-link"></a>
+              {{ props.feed.caption.replace(/•/g, '').replace(/#[a-zA-Z0-9]+/g, '') }}
+              <a
+                :href="props.feed.permalink"
+                rel="noopener"
+                target="_blank"
+                class="stretched-link"
+              />
             </b-card-text>
           </b-card>
           <b-card
@@ -56,112 +81,176 @@
             no-body
             class="mb-2 border-rounded"
           >
-          <b-card-text class="p-2 caption-text">
-            {{props.feed.caption.replace(/•/g, '').replace(/#[a-zA-Z0-9]+/g, '')}}
-            <a :href="props.feed.permalink" rel="noopener" target="_blank" class="stretched-link"></a>
-          </b-card-text>
-        </b-card>
-      </template>
+            <b-card-text class="p-2 caption-text">
+              {{ props.feed.caption.replace(/•/g, '').replace(/#[a-zA-Z0-9]+/g, '') }}
+              <a
+                :href="props.feed.permalink"
+                rel="noopener"
+                target="_blank"
+                class="stretched-link"
+              />
+            </b-card-text>
+          </b-card>
+        </template>
+        <template #error="props">
+          <div class="fancy-alert">
+            {{ props.error }}
+          </div>
+        </template>
+      </insta-feed>
 
-
-      <template v-slot:error="props">
-        <div class="fancy-alert">
-          {{ props.error }}
-        </div>
-      </template>
-    </insta-feed>
-
-    <google-reviews>
-      <template v-slot:loading="props">
-        <h1 v-if="props.loading" class="copy-heading pt-3">Loading recent Google reviews...</h1>
-      </template>
-      <template v-slot:reviews="props">
-        <b-card
-          class="m-1 rounded shadow"
-          v-if="props.index < 3"
+      <google-reviews>
+        <template #loading="props">
+          <h1
+            v-if="props.loading"
+            class="copy-heading pt-3"
           >
-          <b-card-text>
-            <span v-for="n in 5" :key="n">
-              <span v-if="n <= props.review.rating" class="primary-color">★</span>
-            </span>
-          </b-card-text>
-          <b-card-text>
-            {{ props.review.text }}
-          </b-card-text>
-          <b-card-text class="small text-muted font-italic">
-            &mdash;
-            {{props.review.author_name.split(' ')[0][0]}}. {{props.review.author_name.split(' ')[1]}}, {{ props.review.relative_time_description }}</b-card-text>
-        </b-card>
-      </template>
-      <template v-slot:error="props">
-        <div class="fancy-alert">
-          {{ props.error }}
-        </div>
-      </template>
-    </google-reviews>
-    <!-- Instagram and Yelp Logos -->
-    <b-row class="py-3">
-      <b-col md="12">
-        <div class="d-flex justify-content-center">
-          <a href="https://www.instagram.com/endlessphotobooth">
-            <img src="~assets/images/home/instagram.png" alt="Instagram Link" style="width: 45px; height: 45px; top: 8px; position: relative;"/>
-          </a>
-          <a href="https://www.yelp.com/biz/endless-photobooth-oxnard">
-            <img src="~assets/images/home/yelp.png" alt="Yelp Link" class="logo" style="width: 60px; height: 60px; position: relative;"/>
-          </a>
-          <a href="https://www.weddingwire.com/biz/endless-photo-booths-oxnard/3fc52dcdeb1f53d2.html">
-            <img src="~assets/images/home/wedding_wire.png" alt="Wedding Wire Link" class="logo" style="width: 50px; height: 50px; top:5px; right: 4px; position: relative;"/>
-          </a>
-        </div>
-      </b-col>
-    </b-row>
-    <!-- Make a non fluid container for the rest of the content -->
-    <!-- Home info section -->
-    <b-row class="pb-3">
-      <b-col md="12">
-      <h1 class="copy-heading">We provide:</h1>
-      <ul>
-        <li>Service to Ventura County, Los Angeles County and Santa Barbara County for all events!</li>
-        <li>A fun way to remember those special days &mdash; not just for you, but for your family & friends as well!</li>
-        <li>Flexibility for every type of event or occasion you have. If there is a photo booth at a wedding, corporate party, or any event, everyone will want to jump in and take a set of fun pictures that they can take home.</li>
-        <li>Affordable and quality photo booth rentals with many great and amazing options. Different backdrop options including Rose Gold, Red, Silver and more.</li>
-      </ul>
-      </b-col>
-    </b-row>
-    <!-- Package Info Section -->
-    <b-row class="py-3">
-      <h1 class="copy-heading">ALL Photo Booth Packages Include:</h1>
-      <b-col order-md="2" md="7">
-        <picture>
-          <source class="img-fluid" srcset="~assets/images/home/all_include-min.webp" type="image/webp">
-          <img class="img-fluid"
-                src="~assets/images/home/all_include-min.jpg"
-                alt="All Photo Booths Include Props, Different backdrop options, Person to Run Photo Booth, and more!">
-        </picture>
-      </b-col>
-      <b-col order-md="1" md="5">
-        <ul>
-          <li class="mt-0">Props</li>
-          <li>Different backdrop options: Rose Gold, Red, Silver and more</li>
-          <li>2 Strips with 3 Photos Taken</li>
-          <li>Copies for Everyone in Photo</li>
-          <li>Unlimited Use of Booth</li>
-          <li>Open Air Photo Booth</li>
-          <li>Custom Text For Bottom Of Picture</li>
-          <li>Person to Run Photo Booth</li>
-          <li>Drop off and Pickup Included</li>
-        </ul>
-      </b-col>
-    </b-row>
-    <PhotoBoothTypes/>
-    <!-- End Section -->
-    <b-row class="p-4">
-      <b-col>
-        <div class="subtitle"> Ventura Photo Booth Rentals will be a perfect addition to any event.
-        <br>
-        <nuxt-link class="contact-us-link" to="/contact-us">Book your photo booth today!</nuxt-link></div>
-      </b-col>
-    </b-row>
+            Loading recent Google reviews...
+          </h1>
+        </template>
+        <template #reviews="props">
+          <b-card
+            v-if="props.index < 3"
+            class="m-1 rounded shadow"
+          >
+            <b-card-text>
+              <span
+                v-for="n in 5"
+                :key="n"
+              >
+                <span
+                  v-if="n <= props.review.rating"
+                  class="primary-color"
+                >★</span>
+              </span>
+            </b-card-text>
+            <b-card-text>
+              {{ props.review.text }}
+            </b-card-text>
+            <b-card-text class="small text-muted font-italic">
+              &mdash;
+              {{ props.review.author_name.split(' ')[0][0] }}. {{ props.review.author_name.split(' ')[1] }}, {{
+                props.review.relative_time_description
+              }}
+            </b-card-text>
+          </b-card>
+        </template>
+        <template #error="props">
+          <div class="fancy-alert">
+            {{ props.error }}
+          </div>
+        </template>
+      </google-reviews>
+      <!-- Instagram and Yelp Logos -->
+      <b-row class="py-3">
+        <b-col md="12">
+          <div class="d-flex justify-content-center">
+            <a href="https://www.instagram.com/endlessphotobooth">
+              <img
+                src="~assets/images/home/instagram.png"
+                alt="Instagram Link"
+                style="width: 45px; height: 45px; top: 8px; position: relative;"
+              >
+            </a>
+            <a href="https://www.yelp.com/biz/endless-photobooth-oxnard">
+              <img
+                src="~assets/images/home/yelp.png"
+                alt="Yelp Link"
+                class="logo"
+                style="width: 60px; height: 60px; position: relative;"
+              >
+            </a>
+            <a href="https://www.weddingwire.com/biz/endless-photo-booths-oxnard/3fc52dcdeb1f53d2.html">
+              <img
+                src="~assets/images/home/wedding_wire.png"
+                alt="Wedding Wire Link"
+                class="logo"
+                style="width: 50px; height: 50px; top:5px; right: 4px; position: relative;"
+              >
+            </a>
+          </div>
+        </b-col>
+      </b-row>
+      <!-- Make a non fluid container for the rest of the content -->
+      <!-- Home info section -->
+      <b-row class="pb-3">
+        <b-col md="12">
+          <h1 class="copy-heading">
+            We provide:
+          </h1>
+          <ul>
+            <li>Service to Ventura County, Los Angeles County and Santa Barbara County for all events!</li>
+            <li>
+              A fun way to remember those special days &mdash; not just for you, but for your family & friends as well!
+            </li>
+            <li>
+              Flexibility for every type of event or occasion you have. If there is a photo booth at a wedding, corporate
+              party, or any event, everyone will want to jump in and take a set of fun pictures that they can take home.
+            </li>
+            <li>
+              Affordable and quality photo booth rentals with many great and amazing options. Different backdrop options
+              including Rose Gold, Red, Silver and more.
+            </li>
+          </ul>
+        </b-col>
+      </b-row>
+      <!-- Package Info Section -->
+      <b-row class="py-3">
+        <h1 class="copy-heading">
+          ALL Photo Booth Packages Include:
+        </h1>
+        <b-col
+          order-md="2"
+          md="7"
+        >
+          <picture>
+            <source
+              class="img-fluid"
+              srcset="~assets/images/home/all_include-min.webp"
+              type="image/webp"
+            >
+            <img
+              class="img-fluid"
+              src="~assets/images/home/all_include-min.jpg"
+              alt="All Photo Booths Include Props, Different backdrop options, Person to Run Photo Booth, and more!"
+            >
+          </picture>
+        </b-col>
+        <b-col
+          order-md="1"
+          md="5"
+        >
+          <ul>
+            <li class="mt-0">
+              Props
+            </li>
+            <li>Different backdrop options: Rose Gold, Red, Silver and more</li>
+            <li>2 Strips with 3 Photos Taken</li>
+            <li>Copies for Everyone in Photo</li>
+            <li>Unlimited Use of Booth</li>
+            <li>Open Air Photo Booth</li>
+            <li>Custom Text For Bottom Of Picture</li>
+            <li>Person to Run Photo Booth</li>
+            <li>Drop off and Pickup Included</li>
+          </ul>
+        </b-col>
+      </b-row>
+      <PhotoBoothTypes />
+      <!-- End Section -->
+      <b-row class="p-4">
+        <b-col>
+          <div class="subtitle">
+            Ventura Photo Booth Rentals will be a perfect addition to any event.
+            <br>
+            <nuxt-link
+              class="contact-us-link"
+              to="/contact-us"
+            >
+              Book your photo booth today!
+            </nuxt-link>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -173,25 +262,24 @@ import InstaFeed from '~/components/InstaFeed.vue';
 import GoogleReviews from '~/components/GoogleReviews.vue';
 
 export default {
-  head () {
-    return {
-      title: 'ENDLESS Ventura Photo Booth Rentals',
-      meta: [
-        { hid: 'description', name: 'description', content: 'ENDLESS PHOTOBOOTH, Ventura Photo Booth Rentals Home Page' }
-      ]
-    }
-  },
   components: {
     PhotoBoothTypes,
     Navbar,
     InstaFeed,
-    GoogleReviews
-  }
-}
+    GoogleReviews,
+  },
+  head() {
+    return {
+      title: 'ENDLESS Ventura Photo Booth Rentals',
+      meta: [
+        { hid: 'description', name: 'description', content: 'ENDLESS PHOTOBOOTH, Ventura Photo Booth Rentals Home Page' },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-
 $break-small: 600px;
 $break-large: 1000px;
 $grey: #eee;
@@ -242,8 +330,6 @@ $grey: #eee;
   opacity: 0.5;
   z-index: 1;
 }
-
-
 
 .middle-text {
   position: absolute;
@@ -298,7 +384,6 @@ $grey: #eee;
   text-decoration: underline;
 }
 
-
 ul{
   text-align: left;
 }
@@ -306,5 +391,4 @@ ul{
 li{
   margin-top: 10px;
 }
-
 </style>
