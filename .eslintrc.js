3743f33ee230f8b9371dd68ca2aebc4a9aaece9f
Donaven Snowden <donaven.snowden@gmail.com>
2022-12-25 14:18:28 -0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    '@nuxt/eslint-config',
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    semi: 'off',
+  },
+};
