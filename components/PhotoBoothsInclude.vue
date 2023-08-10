e382a149f132a71693b67fe4779f6e23682e4257
Donaven Snowden <donaven.snowden@gmail.com>
2023-08-10 09:48:04 -0700

    Styling changes

---
+++ components/PhotoBoothsInclude.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="pb-5">
+    <h1 class="copy-heading">ALL Photo Booth Packages Include:</h1>
+    <ul>
+      <li>Props</li>
+      <li>Different backdrop options: Rose Gold, Red, Silver and more</li>
+      <li>2 Strips with 3 Photos Taken</li>
+      <li>Copies for Everyone in Photo</li>
+      <li>Unlimited Use of Booth</li>
+      <li>Custom Text For Bottom Of Picture</li>
+      <li>Person to Run Photo Booth</li>
+      <li>Drop off and Pickup Included</li>
+    </ul>
+
+    <picture>
+      <source
+        class="img-fluid"
+        srcset="~assets/images/home/all_include-min.webp"
+        type="image/webp"
+      />
+      <img
+        class="img-fluid"
+        src="~assets/images/home/all_include-min.jpg"
+        alt="All Photo Booth Include Props, Different backdrop options,
+                 Person to Run Photo Booth, and more!"
+      />
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhotoBoothsInclude",
+};
+</script>
+
+<style scoped>
+/* Add your CSS styles here if needed */
+</style>
